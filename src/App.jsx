99a0046ac174988b38fad5524d64d1eecee83388
Anshul-Raghuwanshi <raghuwanshianshul29@gmail.com>
2024-09-18 17:39:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello From React Boiler Plate</h1>
+      <h2>{import.meta.env.VITE_API_KEY}</h2>
+    </div>
+   
+  );
+}
+
+export default App;
+
