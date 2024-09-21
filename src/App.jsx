a45ddf8dd99189d95a911bbec83659dc2dc81d0d
Anshul-Raghuwanshi <raghuwanshianshul29@gmail.com>
2024-09-21 16:43:28 +0530

    Components Creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 function App() {
+  let data = "hello this is data from the server";
+  let subject = [ "hindi", "english", "math"];
+  let profile = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+  };
   return (
-    <div>
-      <h1>Hello From React Boiler Plate</h1>
-      <h2>{import.meta.env.VITE_API_KEY}</h2>
-    </div>
-   
-  );
-}
+      <div>
+        <Home/>
+        <About/>
+        <Nav/>
+        <h1>{data}</h1>
+        <h1>{subject}</h1>
+        <h1>{profile.name}</h1>
+        <h1>{profile.age}</h1>
+        <h1>{profile.city}</h1>
+      </div>  
+)}
 
-export default App;
-
+export default App
